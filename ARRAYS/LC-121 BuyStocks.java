ba1b05e75ccc5c506a75ee720e8d29a5b6a3d3fe
Abraruddin mohammed <uddinabrar0824@gmail.com>
2026-01-24 23:03:40 +0530

    Add solution for Best Time to Buy and Sell Stock
    Implemented a solution to find the maximum profit from a single buy-sell transaction of stocks.

---
+++ ARRAYS/LC-121 BuyStocks.java
@@ -0,0 +1,46 @@
+// Problem: Best Time to Buy and Sell Stock
+// Platform: LeetCode / DSA Practice
+// Description: Find the maximum profit from one buy-sell transaction.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices) {
+            if(price < minPrice) {
+                minPrice = price;  // update buying price
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input: size
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        // Input: stock prices
+        for(int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        Solution sol = new Solution();
+        int profit = sol.maxProfit(prices);
+
+        // Output: max profit
+        System.out.println(profit);
+    }
+}
